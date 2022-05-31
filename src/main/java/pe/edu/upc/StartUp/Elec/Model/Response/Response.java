5bcbaa88d068587c3a79b2ba1c44dc1edf655521
Rodrigo Nole Berrocal <u201816249@upc.edu.pe>
2022-05-31 01:27:04 -0500

    Creating Model.Response

---
+++ src/main/java/pe/edu/upc/StartUp/Elec/Model/Response/Response.java
@@ -0,0 +1,17 @@
+package pe.edu.upc.StartUp.Elec.Model.Response;
+
+import lombok.*;
+
+@Getter
+@Setter
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Response {
+
+
+    private String code;
+    private String message;
+
+}
