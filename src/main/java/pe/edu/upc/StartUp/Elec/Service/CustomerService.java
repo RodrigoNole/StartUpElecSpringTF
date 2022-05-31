8afe4e2449673e41a82d724dde8c20affe4cdde1
Rodrigo Nole Berrocal <u201816249@upc.edu.pe>
2022-05-31 01:28:32 -0500

    Creating Service

---
+++ src/main/java/pe/edu/upc/StartUp/Elec/Service/CustomerService.java
@@ -0,0 +1,17 @@
+package pe.edu.upc.StartUp.Elec.Service;
+
+import pe.edu.upc.StartUp.Elec.Model.Entity.Customer;
+
+import java.util.List;
+
+public interface CustomerService {
+    public List<Customer> getAll();
+
+    public List<Customer> findByDni(Integer dni);
+    public Customer save (Customer customer);
+
+    public Customer update (Customer customer);
+
+    public void deleteById (Integer id);
+
+}
