46aeec5471cd389287dd866da2e3212db70ae4e5
Rodrigo Nole Berrocal <u201816249@upc.edu.pe>
2022-05-31 00:53:15 -0500

    first commit

---
+++ src/main/java/pe/edu/upc/StartUp/Elec/StartUpElecApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.upc.StartUp.Elec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StartUpElecApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StartUpElecApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pe/edu/upc/StartUp/Elec/StartUpElecApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.upc.StartUp.Elec;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StartUpElecApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
