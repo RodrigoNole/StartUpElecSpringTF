4c11d8db4aefb9b4a0565f41ce87ce8be8ff4f34
lordgius <lg190799@hotmail.com>
2022-06-05 02:48:08 -0500

    listo

--- src/main/java/pe/edu/upc/StartUp/Elec/business/crud/EmployeeService.java
+++ src/main/java/pe/edu/upc/StartUp/Elec/business/crud/EmployeeService.java
@@ -1,14 +1,13 @@
 package pe.edu.upc.StartUp.Elec.Business.Crud;
 
-
+import pe.edu.upc.StartUp.Elec.Model.Entity.Benefit;
 import pe.edu.upc.StartUp.Elec.Model.Entity.Employee;
 
 import java.util.List;
 
-public interface EmployeeService extends CrudService<Employee, Integer>{
+public interface EmployeeService {
 
     public List<Employee> getAll();
-    
     public List<Employee> findByDni(Integer dni);
 
     public Employee save (Employee employee);
